7832e0afca627d808a02ecb97833e91a47f8475d
yusukemorita <yusuke.ppq@gmail.com>
2018-09-19 23:18:40 +0900

    measure time in Exercise 3.4

--- exercises/src/test/scala/fpinscala/datastructures/Exercise3_4Spec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/Exercise3_4Spec.scala
@@ -1,6 +1,7 @@
 package fpinscala.datastructures
 
 import org.scalatest._
+import fpinscala.ExecutionTimeMeasurer.measureTime
 
 class Exercise3_4Spec extends FlatSpec {
 
@@ -8,12 +9,16 @@ class Exercise3_4Spec extends FlatSpec {
     (List(1, 2, 3, 4), 2, List(3, 4)),
     (List(1, 2, 3, 4), 3, List(4)),
     (List(1, 2, 3, 4), 4, Nil),
-    (Nil, 0, Nil)
+    (Nil, 0, Nil),
+    (List(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0), 1, List(2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0)),
+    (List(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0), 10, List(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)),
+    (List(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0), 20, Nil)
   )
 
   for ((list, number, output) <- passingTestCases) {
     s"drop $number of $list" should s"return $output" in {
-      assert(List.drop(list,  number) === output)
+      val resultList = measureTime{ List.drop(list,  number)}
+      assert(resultList === output)
     }
   }
 
