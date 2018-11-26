381ff9f60f234e4863794d8fd50443c79fd55aa9
yusukemorita <yusuke.ppq@gmail.com>
2018-11-26 20:06:35 +0900

    Exercise6.2

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -40,7 +40,10 @@ object RNG {
     (if (n < 0) -(n+1) else n, nextRng)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (n, nextRng) = nonNegativeInt(rng)
+    (n / (Int.MaxValue.toDouble + 1), nextRng)
+  }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = ???
 
