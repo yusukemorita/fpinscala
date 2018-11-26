b22ef5d41ce74c595ee47eebe6d9cef9aebfbba4
yusukemorita <yusuke.ppq@gmail.com>
2018-11-26 20:20:26 +0900

    Exercise6.3

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -45,11 +45,23 @@ object RNG {
     (n / (Int.MaxValue.toDouble + 1), nextRng)
   }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i1, rng1) = rng.nextInt
+    val (d1, rng2) = double(rng1)
+    ((i1, d1), rng2)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), nextRng) = intDouble(rng)
+    ((d, i), nextRng)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
+  }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
 
