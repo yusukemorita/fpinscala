5782cb4f90e2cec177ec0e895eed4b52c84d8df8
yusukemorita <yusuke.ppq@gmail.com>
2018-12-02 11:53:50 +0900

    Exercise 7.3

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -11,10 +11,37 @@ object Par {
   def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a) // `unit` is represented as a function that returns a `UnitFuture`, which is a simple implementation of `Future` that just wraps a constant value. It doesn't use the `ExecutorService` at all. It's always done and can't be cancelled. Its `get` method simply returns the value that we gave it.
   
   private case class UnitFuture[A](get: A) extends Future[A] {
-    def isDone = true 
+    def isDone = true
     def get(timeout: Long, units: TimeUnit) = get 
     def isCancelled = false 
     def cancel(evenIfRunning: Boolean): Boolean = false 
+  }
+
+  private case class Map2Future[A,B,C](a: Future[A], b: Future[B])(f: (A,B) => C) extends Future[C] {
+    @volatile var cache: Option[C] = None
+
+    def cancel(mayInterruptIfRunning: Boolean): Boolean =
+      a.cancel(mayInterruptIfRunning) && b.cancel(mayInterruptIfRunning)
+
+    def isCancelled: Boolean = a.isCancelled || b.isCancelled
+
+    def isDone: Boolean = cache.isDefined
+
+    def get(): C = compute(Long.MaxValue)
+
+    def get(timeout: Long, unit: TimeUnit): C = compute(TimeUnit.NANOSECONDS.convert(timeout, unit))
+
+    def compute(timeoutInNanos: Long): C = cache match {
+      case Some(c) => c
+      case None =>
+        val startTime = System.nanoTime()
+        val aa = a.get(timeoutInNanos, TimeUnit.NANOSECONDS)
+        val endTime = System.nanoTime()
+        val bb = b.get(timeoutInNanos - (endTime - startTime), TimeUnit.NANOSECONDS)
+        val result = f(aa, bb)
+        cache = Some(result)
+        result
+    }
   }
   
   def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
@@ -23,7 +50,14 @@ object Par {
       val bf = b(es)
       UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
     }
-  
+
+  def map2WithTimeout[A,B,C](a: Par[A], b: Par[B], timeout: Long, timeUnit: TimeUnit)(f: (A,B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      Map2Future(af, bf)(f)
+    }
+
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es => es.submit(new Callable[A] { 
       def call = a(es).get
